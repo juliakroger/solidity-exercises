6f4abe6da1c8c22c64e84a98f785997fb1270f2a
Júlia Kroger <37405043+juliakroger@users.noreply.github.com>
2023-01-28 16:56:35 -0500

    Update debug-assignment.sol

--- debug-assignment.sol
+++ debug-assignment.sol
@@ -1,24 +1,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- */
 contract Storage {
-    uint number;
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
+    uint256 number;
+
     function store(uint256 num) public {
         number = num;
     }
 
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
+    function retrieve() public view returns (uint256) {
         return number;
     }
 }
